14cff5d97e0ad10617d0fe77dfeae9136c52510b
kosmoSAR <santiagoaristizabal.kosmetikon@gmail.com>
2023-07-24 14:29:25 -0500

    elimino el localStorage

--- src/app/formato-numero/formato-numero.component.ts
+++ src/app/formato-numero/formato-numero.component.ts
@@ -7,16 +7,12 @@ import { DlgModifyComponent } from './components/dlg-modify/dlg-modify.component
   templateUrl: './formato-numero.component.html',
   styleUrls: ['./formato-numero.component.css']
 })
-export class FormatoNumeroComponent implements OnInit {
+export class FormatoNumeroComponent {
 
   public principal_number: string = "1.000,54";
   formato: number = 1;
 
   constructor(private _dialog: MatDialog){}
-
-  ngOnInit(): void {
-    this.loadLocalStorage()
-  }
 
   openModifyDialog(){
     const dialogRef = this._dialog.open(DlgModifyComponent);
@@ -27,19 +23,9 @@ export class FormatoNumeroComponent implements OnInit {
           console.log(value);
           this.principal_number = value.number;
           this.formato = value.format;
-          this.saveLocalStorage();
         }
       }
     )
-  }
-
-  private saveLocalStorage(): void{
-    localStorage.setItem('number', JSON.stringify(this.principal_number))
-  }
-
-  private loadLocalStorage(): void{
-    if ( !localStorage.getItem('number') ) return;
-    this.principal_number = JSON.parse(localStorage.getItem('number')!)
   }
 
 }
